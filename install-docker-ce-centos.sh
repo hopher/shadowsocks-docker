aa9ad9436d8d8f99444141647a565d4a27acdc1b
hopher <565983236@qq.com>
2019-04-15 22:51:55 +0800

    update files 2019年 4月15日 星期一 22时51分55秒 CST

--- install-docker-ce-centos.sh
+++ install-docker-ce-centos.sh
@@ -22,6 +22,7 @@ yum install docker-ce docker-ce-cli containerd.io
 
 ## 安装 docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 ## 启动 docker 服务 && 开机启动
 service docker restart
